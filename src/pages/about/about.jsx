b153adf55bc7737332488f76c9d511b016bd4459
McPrds <mcparedes2028@gmail.com>
2023-07-25 01:43:00 +0800

    about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,107 +1,97 @@
-import { Box, Text} from "@chakra-ui/react";
+import { Box, Text, useMediaQuery } from "@chakra-ui/react";
 import aboutImage from "../../assets/aboutimage.webp";
-import blob from "../../assets/blob.svg"
+import blob from "../../assets/blob.svg";
 
 export const About = () => {
+  const [isMobileScreen] = useMediaQuery("(max-width: 40em)");
+  const [isTabletScreen] = useMediaQuery("(max-width: 768px)");
+
   return (
-    <Box overflow="auto">
-        
-          <Box mt="150" w="40rem" ml="20px"> 
-          <img src = {aboutImage}
-          alt="Image 1"
-          height="250px"
-          width="250px"/>
-          </Box>
-
-          <Box mt="-200px" w="30rem" ml="300px">
-          <img src = {aboutImage}
-          alt="Image 2"
-          height="250px"
-          width="250px"/>
-          </Box>
-
-          <Box mt="-30px" w="30rem" ml="20px">
-          <img src = {aboutImage}
-          alt="Image 3"
-          height="250px"
-          width="250px"/>
-          </Box>
-
-          <Box mt="-200px" w="30rem" ml="300px">
-          <img src = {aboutImage}
-          alt="Image 4"
-          height="250px"
-          width="250px"/>
-          </Box>
-
-          <Box mt="-600px" w="30rem" ml="600px">
-          <Text
-            position="absolute"
-            fontSize="50px"
-            fontWeight="semibold"
-            justifyContent="center"
-            alignItems="center"
-            fontFamily="Abril-Fatface">
-            About <span style={{color:"#FFC700"}}>Us</span>
-            <hr style={{ border: "1px solid black", width: "650px", margin: "10px auto" }}/>
-          </Text>
-          </Box>
-
-          <Box mt="100px" w="30rem" ml="600px">
-          <Text
-            position="absolute"
-            fontSize="25px"
-            fontWeight="light"
-            justifyContent="center"
-            alignItems="center"
-            fontFamily="Abril-Fatface">
-            Quezoncrave is born out of a strong passion for the flavors, traditions, and rich food culture of Quezon Province. 
-            The journey began with a simple longing for the tastes of home that couldn't be satisfied elsewhere. 
-            Thus, the mission was born to bring the authentic flavors of Quezon to food enthusiasts both near and far. 
-            Beyond tantalizing taste buds, the goal is to preserve and honor the culinary heritage of Quezon Province. 
-            This entails deep respect for local farmers, fishermen, and artisans who provide fresh ingredients, and their hard work is showcased in every dish.
-            By staying true to Quezon cuisine's authenticity, the aim is to contribute to the sustainability and appreciation of the local food culture.
-          </Text>
-        </Box>
-
-        <Box mt="700" w="40rem" ml="20px">
-          <Text
-            position="absolute"
-            fontSize="50px"
-            fontWeight="semibold"
-            justifyContent="center"
-            alignItems="center"
-            fontFamily="Abril-Fatface">
-            You Crave ?<br></br><span style={{color:"#FFC700"}}>We Cook</span>
-            <hr style={{ border: "1px solid black", width: "500px", margin: "10px auto" }}/>
-          </Text>
-        </Box>
-
-        {/* Blob Image */}
-      <Box
-        position="absolute"
-        top="800px" // Adjust the top position as per your requirement
-        left="700px" // Adjust the left position as per your requirement
-        zIndex="1" // Set a higher zIndex to make it appear in front of other elements
-      >
-        <img src={blob} alt="Blob Image" height="500px" width="500px" />
+    <Box w="100%" h="auto">
+      <Box mt="120px" w={isMobileScreen ? "100%" : "40rem"} ml={isMobileScreen ? "120px" : "20px"}>
+        <img src={aboutImage} alt="Image 1" height="250px" width="250px" />
       </Box>
 
-           <Box mt="900px" w="30rem" ml="20px">
-          <Text
-            position="absolute"
-            fontSize="25px"
-            fontWeight="light"
-            justifyContent="center"
-            alignItems="center"
-            fontFamily="Abril-Fatface"
-            maxW="700">
-            At Quezoncrave, we understand that there's nothing quite like the taste of home. 
-            If you find yourself craving the authentic flavors of Quezon Province, look no further. 
-            We are here to bring the deliciousness of Quezon cuisine directly to your table.
-            </Text>
-        </Box>  
+      <Box mt={isMobileScreen ? "10px" : "-200px"} w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "120px" : "300px"}>
+        <img src={aboutImage} alt="Image 2" height="250px" width="250px" />
+      </Box>
 
+      <Box mt="10px" w={isMobileScreen ? "100%" : "40rem"} ml={isMobileScreen ? "120px" : "20px"}>
+        <img src={aboutImage} alt="Image 1" height="250px" width="250px" />
+      </Box>
+
+      <Box mt={isMobileScreen ? "10px" : "-200px"} w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "120px" : "300px"}>
+        <img src={aboutImage} alt="Image 4" height="250px" width="250px" />
+      </Box>
+
+      <Box mt={isMobileScreen ? "20px" : "-600px"} w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "185px" : "600px"}>
+        <Text
+          position="relative"
+          fontSize={isMobileScreen ? "30px" : "50px"}
+          fontWeight="semibold"
+          justifyContent="center"
+          alignItems="center"
+          fontFamily="Abril-Fatface"
+        >
+          About <span style={{ color: "#FFC700" }}>Us</span>
+          <hr style={{ border: "1px solid black", width: isMobileScreen ? "200px" : "650px", margin: "10px -30px -70px" }}/>
+        </Text>
+      </Box>
+
+      <Box mt="100px" w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "0" : "650px"}>
+        <Text
+          position="relative"
+          fontSize={isMobileScreen ? "20px" : isTabletScreen ? "22px" : "20px"}
+          fontWeight="light"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+          fontFamily="Abril-Fatface"
+          maxW={isMobileScreen ? "100%" : "700px"}
+        >
+          Quezoncrave is born out of a strong passion for the flavors, traditions, and rich food culture of Quezon Province.
+          The journey began with a simple longing for the tastes of home that couldn't be satisfied elsewhere.
+          Thus, the mission was born to bring the authentic flavors of Quezon to food enthusiasts both near and far.
+          Beyond tantalizing taste buds, the goal is to preserve and honor the culinary heritage of Quezon Province.
+          This entails deep respect for local farmers, fishermen, and artisans who provide fresh ingredients, and their hard work is showcased in every dish.
+          By staying true to Quezon cuisine's authenticity, the aim is to contribute to the sustainability and appreciation of the local food culture.
+        </Text>
+      </Box>
+
+      <Box mt={isMobileScreen ? "20px" : "230px"} w={isMobileScreen ? "100%" : "40rem"} ml={isMobileScreen ? "185px" : "30px"}>
+        <Text
+          position="relative"
+          fontSize={isMobileScreen ? "30px" : "50px"}
+          fontWeight="semibold"
+          justifyContent="center"
+          alignItems="center"
+          fontFamily="Abril-Fatface"
+        >
+          You Crave ?<br /><span style={{ color: "#FFC700" }}>We Cook</span>
+          <hr style={{ border: "1px solid black", width: isMobileScreen ? "200px" : "650px", margin: "10px -30px -70px" }}/>
+        </Text>
+      </Box>
+
+      <Box position="relative" top={isMobileScreen ? "30px" : "-100px"} left={isMobileScreen ? "0" : "700px"}>
+        <img src={blob} alt="Blob Image" height={isMobileScreen ? "400px" : "500px"} width={isMobileScreen ? "100%" : "500px"} />
+      </Box>
+
+      <Box mt={isMobileScreen ? "-50px" : "-400px"} w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "0" : "50px"}>
+        <Text
+          position="relative"
+          fontSize={isMobileScreen ? "20px" : "20px"}
+          fontWeight="light"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+          fontFamily="Abril-Fatface"
+          maxW={isMobileScreen ? "100%" : "700px"}
+        >
+          At Quezoncrave, we understand that there's nothing quite like the taste of home.
+          If you find yourself craving the authentic flavors of Quezon Province, look no further.
+          We are here to bring the deliciousness of Quezon cuisine directly to your table.
+        </Text>
+      </Box>
     </Box>
   );
 };
