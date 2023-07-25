d59a904162b56493656b1578341c70af3c69cf33
McPrds <mcparedes2028@gmail.com>
2023-07-25 13:18:42 +0800

    about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -38,14 +38,14 @@ export const About = () => {
           <hr
             style={{
               border: "1px solid black",
-              width: isMobileScreen ? "200px" : "650px",
+              width: isMobileScreen ? "200px" : "500px",
               margin: "10px auto",
             }}
           />
         </Text>
       </Box>
 
-      <Box mt="20px" w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "0" : "700px"}>
+      <Box mt="20px" w={isMobileScreen ? "100%" : "30rem"} ml={isMobileScreen ? "0" : "650px"}>
         <Text
           position="relative"
           fontSize={isMobileScreen ? "20px" : isTabletScreen ? "22px" : "20px"}
@@ -54,7 +54,7 @@ export const About = () => {
           alignItems="center"
           textAlign="center"
           fontFamily="Abril-Fatface"
-          maxW={isMobileScreen ? "100%" : "700px"}
+          maxW={isMobileScreen ? "100%" : "400px"}
         >
           Quezoncrave is born out of a strong passion for the flavors, traditions, and rich food culture of Quezon Province.
           The journey began with a simple longing for the tastes of home that couldn't be satisfied elsewhere.
@@ -65,14 +65,14 @@ export const About = () => {
         </Text>
       </Box>
 
-      <Box mt={isMobileScreen ? "20px" : "230px"} w={isMobileScreen ? "100%" : "40rem"} ml={isMobileScreen ? "0px" : "30px"}>
+      <Box mt={isMobileScreen ? "20px" : "150px"} w={isMobileScreen ? "100%" : "40rem"} ml={isMobileScreen ? "0px" : "30px"}>
         <Text
           position="relative"
           fontSize={isMobileScreen ? "30px" : "50px"}
           fontWeight="semibold"
           justifyContent="center"
           alignItems="center"
-          textAlign="center"
+          textAlign={isMobileScreen ? "center":""}
           fontFamily="Abril-Fatface"
         >
           You Crave ?<br /><span style={{ color: "#FFC700" }}>We Cook</span>
@@ -99,7 +99,7 @@ export const About = () => {
           alignItems="center"
           textAlign="center"
           fontFamily="Abril-Fatface"
-          maxW={isMobileScreen ? "100%" : "700px"}
+          maxW={isMobileScreen ? "100%" : "400px"}
         >
           At Quezoncrave, we understand that there's nothing quite like the taste of home.
           If you find yourself craving the authentic flavors of Quezon Province, look no further.
