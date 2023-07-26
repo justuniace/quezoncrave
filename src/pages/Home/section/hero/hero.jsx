66641e07050f10097df44c5b0485496041dbdb40
Eunice <valdezeunice804@gmail.com>
2023-07-26 11:13:29 +0800

    asj

--- src/pages/Home/section/hero/hero.jsx
+++ src/pages/Home/section/hero/hero.jsx
@@ -6,8 +6,7 @@ import {
   VStack,
   HStack,
   Highlight,
-  useMediaQuery
- 
+  useMediaQuery,
 } from "@chakra-ui/react";
 
 import CustomButton from "../../../components/buttons";
@@ -18,103 +17,96 @@ import L1 from "../../../../assets/hero/leaves1.webp";
 import L2 from "../../../../assets/hero/leaves2.webp";
 import Pork from "../../../../assets/hero/hero.svg";
 import Bg from "../../../../assets/hero/b1.webp";
-import { motion } from "framer-motion";
-import useObserver from "../../../components/Observer/useObserver"
+// import { motion } from "framer-motion";
+//import {Reveal} from "../../../components/Observer/useObserver"
 
 export default function Hero() {
-
   const [isMobileScreen] = useMediaQuery("(max-width: 40em)");
-  const { ref, isInView } = useObserver();
+  // const { ref, isInView } = useObserver();
 
-  const slideInAnimation = {
-    hidden: { opacity: 0, y: 30 },
-    visible: { opacity: 1, y: 0 },
-  };
+  // const slideInAnimation = {
+  //   hidden: { opacity: 0, y: 30 },
+  //   visible: { opacity: 1, y: 0 },
+  // };
 
   return (
-    <motion.div
-      ref={ref}
-      initial="hidden"
-      animate={isInView ? "visible" : "hidden"}
-      variants={slideInAnimation}
-      transition={{ duration: 0.6 }}
+    <Flex
+      h="100%"
+      w="100%"
+      position="relative"
+      bg="#262424"
+      justifyContent="center"
+      alignItems="center"
+      top="0"
+      bottom="0"
+      left="0"
+      right="0"
     >
-      <Flex
-        h="100%"
-        w="100%"
-        position="relative"
-        bg="#262424"
-        justifyContent="center"
-        alignItems="center"
-        top="0"
-        bottom="0"
-        left="0"
-        right="0"
-      >
-        <Box>
-          {/* background image right*/}
-          <Image
-            width="8rem"
-            src={Bg}
-            position="relative"
-            left="50%"
-            top="0"
-            h="50rem"
-            w="40rem"
-            opacity="0.1"
-          />
-        </Box>
-        <Box>
-          {/* background image right*/}
-          <Image
-            width="8rem"
-            src={Bg}
-            position="relative"
-            left="70%"
-            top="0"
-            h="50rem"
-            w="40rem"
-            opacity="0.1"
-          />
-        </Box>
-        <Box>
-          {/* background image left */}
-          <Image
-            width="8rem"
-            src={Bg}
-            position="absolute"
-            right="63%"
-            top="12"
-            h="50rem"
-            w="40rem"
-            opacity="0.1"
-          />
-        </Box>
-        {/* leaves right */}
-        <Box>
-          <Image
-            src={L2}
-            position="absolute"
-            top={isMobileScreen ? "16%" : "25%"}
-            left={isMobileScreen ? "80%" : "78%"}
-            w="20%"
-            transform="rotate(150deg)"
-          />
-        </Box>
-        {/* leaves left */}
-        <Box>
-          <Image
-            src={L2}
-            position="absolute"
-            top={isMobileScreen ? "23%" : "16%"}
-            left={isMobileScreen ? "59%" : "45%"}
-            w={isMobileScreen ? "20%" : "20%"}
-            transform="rotate(20deg)"
-            zIndex=""
-          />
-        </Box>
+      <Box>
+        {/* background image right*/}
+        <Image
+          width="8rem"
+          src={Bg}
+          position="relative"
+          left="50%"
+          top="0"
+          h="50rem"
+          w="40rem"
+          opacity="0.1"
+        />
+      </Box>
+      <Box>
+        {/* background image right*/}
+        <Image
+          width="8rem"
+          src={Bg}
+          position="relative"
+          left="70%"
+          top="0"
+          h="50rem"
+          w="40rem"
+          opacity="0.1"
+        />
+      </Box>
+      <Box>
+        {/* background image left */}
+        <Image
+          width="8rem"
+          src={Bg}
+          position="absolute"
+          right="63%"
+          top="12"
+          h="50rem"
+          w="40rem"
+          opacity="0.1"
+        />
+      </Box>
+      {/* leaves right */}
+      <Box>
+        <Image
+          src={L2}
+          position="absolute"
+          top={isMobileScreen ? "16%" : "25%"}
+          left={isMobileScreen ? "80%" : "78%"}
+          w="20%"
+          transform="rotate(150deg)"
+        />
+      </Box>
+      {/* leaves left */}
+      <Box>
+        <Image
+          src={L2}
+          position="absolute"
+          top={isMobileScreen ? "23%" : "16%"}
+          left={isMobileScreen ? "59%" : "45%"}
+          w={isMobileScreen ? "20%" : "20%"}
+          transform="rotate(20deg)"
+          zIndex=""
+        />
+      </Box>
 
-        {/* hero-image */}
+      {/* hero-image */}
+      {/* <Reveal width="fit-content"> */}
         <Box w={isMobileScreen ? "13rem" : ""}>
           <Image
             src={Pork}
@@ -125,78 +117,78 @@ export default function Hero() {
             zIndex="1"
           />
         </Box>
-        {/* leaves1 bottom */}
-        <Box>
-          <Image
-            src={L1}
-            position="absolute"
-            left={isMobileScreen ? "80%" : "72%"}
-            top={isMobileScreen ? "28%" : "64%"}
-            w={isMobileScreen ? "36%" : "50%"}
-            zIndex="2"
-          />
-        </Box>
-
-        {/* Hero Text */}
-        <Flex
-          left="10%"
-          top={isMobileScreen ? "15%" : "24%"}
+      {/* </Reveal> */}
+      {/* leaves1 bottom */}
+      <Box>
+        <Image
+          src={L1}
           position="absolute"
-          w="40rem"
-        >
-          <VStack align="start" w={{ base: "100%", md: "55%" }}>
-            <Box w={isMobileScreen ? "10rem" : "40rem"}>
-              <Text
-                lineHeight={isMobileScreen ? "2rem" : "120px"}
-                color="white"
-                fontSize={isMobileScreen ? "2rem" : "6rem"}
-                fontWeight="semi-bold"
-                fontFamily="Abril-Fatface"
-              >
-                <Highlight
-                  query="Cravings."
-                  styles={{
-                    color: "#FFC700",
-                  }}
-                >
-                  Satisfy Your Cravings.
-                </Highlight>
-              </Text>
-            </Box>
+          left={isMobileScreen ? "80%" : "72%"}
+          top={isMobileScreen ? "28%" : "64%"}
+          w={isMobileScreen ? "36%" : "50%"}
+          zIndex="2"
+        />
+      </Box>
 
-            <Box w={isMobileScreen ? "10rem" : "30rem"}>
-              <Text
-                color="white"
-                marginTop={isMobileScreen ? "45px" : "30px"}
-                fontFamily="inter"
-                fontWeight="medium"
-                fontSize={isMobileScreen ? "12px" : "18px"}
+      {/* Hero Text */}
+      <Flex
+        left="10%"
+        top={isMobileScreen ? "15%" : "24%"}
+        position="absolute"
+        w="40rem"
+      >
+        <VStack align="start" w={{ base: "100%", md: "55%" }}>
+          <Box w={isMobileScreen ? "10rem" : "40rem"}>
+            <Text
+              lineHeight={isMobileScreen ? "2rem" : "120px"}
+              color="white"
+              fontSize={isMobileScreen ? "2rem" : "6rem"}
+              fontWeight="semi-bold"
+              fontFamily="Abril-Fatface"
+            >
+              <Highlight
+                query="Cravings."
+                styles={{
+                  color: "#FFC700",
+                }}
               >
-                Delight Your Palate with Quezon`s Finest. {""}
-                {/* <Icon as={BiBowlHot} /> */}
-              </Text>
-            </Box>
-            <Box mt="50px" zIndex={1}>
-              <HStack spacing={isMobileScreen ? "10rem" : "15rem"}>
-                <CustomButton
-                  icon={<FaOpencart />}
-                  buttonText="Order Now!"
-                  buttonLink="/menu"
-                  width={isMobileScreen ? "10rem" : "13rem"}
-                />
-                <CustomButton
-                  icon={<SlMagnifier />}
-                  buttonLink="/blog"
-                  buttonText="Explore"
-                  hasBorder={false}
-                  isYellowButton={true}
-                  width={isMobileScreen ? "1.2rem" : "13rem"}
-                />
-              </HStack>
-            </Box>
-          </VStack>
-        </Flex>
+                Satisfy Your Cravings.
+              </Highlight>
+            </Text>
+          </Box>
+
+          <Box w={isMobileScreen ? "10rem" : "30rem"}>
+            <Text
+              color="white"
+              marginTop={isMobileScreen ? "45px" : "30px"}
+              fontFamily="inter"
+              fontWeight="medium"
+              fontSize={isMobileScreen ? "12px" : "18px"}
+            >
+              Delight Your Palate with Quezon`s Finest. {""}
+              {/* <Icon as={BiBowlHot} /> */}
+            </Text>
+          </Box>
+          <Box mt="50px" zIndex={1}>
+            <HStack spacing={isMobileScreen ? "10rem" : "15rem"}>
+              <CustomButton
+                icon={<FaOpencart />}
+                buttonText="Order Now!"
+                buttonLink="/menu"
+                width={isMobileScreen ? "10rem" : "13rem"}
+              />
+              <CustomButton
+                icon={<SlMagnifier />}
+                buttonLink="/blog"
+                buttonText="Explore"
+                hasBorder={false}
+                isYellowButton={true}
+                width={isMobileScreen ? "1.2rem" : "13rem"}
+              />
+            </HStack>
+          </Box>
+        </VStack>
       </Flex>
-    </motion.div>
+    </Flex>
   );
 }
